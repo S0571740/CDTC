cee2ff52b1b0066c2f289103d63aaacee2b8244b
S0571740 <philip.blankenburg@student.htw-berlin.de>
2022-03-24 18:12:56 +0100

    fill audiocontroller

--- Assets/Scripts/Controller/AudioController.cs
+++ Assets/Scripts/Controller/AudioController.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class AudioController : MonoBehaviour
 {
+    [SerializeField] private AudioClip menuMusic;
+    [SerializeField] private AudioClip gameMusic;
+    [SerializeField] private AudioSource source;
 
+    void Start(){
+        source.clip = menuMusic;
+        source.Play();
+    }
+
+    public void disable(){
+        source.Stop();
+    }
+
+    public void enable(){
+        source.Play();
+    }
+
+    public void switchTrack(){
+        source.clip = gameMusic;
+    }
 }
